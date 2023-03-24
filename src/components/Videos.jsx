7e3a3241f2d257b5339ae377d42bdb418fd9f071
NoobSeeker <I.really.need.cloud@gmail.com>
2023-03-24 18:24:08 +0100

    Added Feed and SideBar

---
+++ src/components/Videos.jsx
@@ -0,0 +1,9 @@
+import { Stack } from '@mui/material';
+
+const Videos = () => (
+    <Stack direction="row" sx={{ overflowY: 'auto', height: { sx: 'auto', md: '95%' }, flexDirection: { md: 'column' }}}>
+       
+    </Stack>
+)
+
+export default Videos;
