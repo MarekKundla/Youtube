cf0be0ff9f725e6fdeec2354fb033329dc3288d9
Marek Kundla <Marek.Kundla@gmail.com>
2023-04-18 11:50:06 +0200

    video detail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -22,8 +22,9 @@ const ChannelDetail = () => {
                 <ChannelCard channelDetail={channelDetail} marginTop = '-110px' />
             </Box>
             <Box display='flex' p='2'>
-                <Box sx={{ mr: { sm: '100px' } }} />
-                <Videos Videos={videos} />
+                <Box sx={{ mr: { sm: '100px' } }}>
+                    <Videos videos={videos} />
+                </Box>
             </Box>
         </Box>
     )
